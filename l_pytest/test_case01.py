5da13d26425c0bfb982a9a7fc5650798f849a9a4
v993611.com <https://mail.163.com/>
2019-05-01 23:08:06 +0800

    unittest practice

--- l_pytest/test_case01.py
+++ l_pytest/test_case01.py
@@ -1,31 +1,29 @@
 import pytest
+import time
+@pytest.fixture(scope='class')
+def haha():
+    print('开始测试')
+    a = '5'
+    yield a
+    print('测试结束')
 
 
 
 class Test_Case(object):
-    @pytest.fixture(scope='class')
-    def setup(self):
-        print('开始测试!!!!!')
-    def a(self):
-        self.a = '5'
-        return self.a
-    def test_case1(self):
-        print(self.a,)
-        assert self.a() =='5','不是5'
 
-    def test_case2(self):
-        assert self.a() =='2','不是2'
+    def test_case(self,haha):
+       # print(self.a,)
+        time.sleep(1)
+        assert haha =='4','不是5'
+
+    def test_case2(self,haha):
+        assert haha =='2','不是2'
 
     def test_case3(self):
-        assert self.a() == '1', '不是1'
+        assert '2' == '1', '不是1'
 
-    @pytest.fixture()
-    def tear_down(self):
-        print('结束测试!!!!')
 
 
 
 if __name__ == '__main__':
-    p =Test_Case()
-    print(p.a()=='5')
-    pytest.main()
+    pytest.main(['C:/Users/Administrator/PycharmProjects/untitled/unit_practice','--junitxml=./reports.xml'])
